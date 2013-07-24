7bc9c0c8745b4375297f38a0cb0c83777e4d141d
Alex Polvi <alex@polvi.net>
2013-07-23 22:47:56 -0700

    feat(coreos-base/oem-ami): add packages to make amis work

--- coreos-base/oem-ami/files/install-ec2-key.sh
+++ coreos-base/oem-ami/files/install-ec2-key.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+/usr/bin/block-until-url http://169.254.169.254/
+
 USER_DIR="/home/core/user"
 
 if [ ! -d ${USER_DIR}/.ssh ] ; then
@@ -7,10 +9,14 @@ if [ ! -d ${USER_DIR}/.ssh ] ; then
 	chmod 700 ${USER_DIR}/.ssh
 fi
 # Fetch public key using HTTP
-curl -s http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key > /tmp/my-key
+curl -s --connect-timeout 3 http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key > /tmp/my-key
 if [ $? -eq 0 ] ; then
 	cat /tmp/my-key >> ${USER_DIR}/.ssh/authorized_keys
 	chmod 700 ${USER_DIR}/.ssh/authorized_keys
 	rm /tmp/my-key
+else
+	echo unable to download key
+	rm /tmp/my-key
+	exit 1
 fi
 chown -R core: $USER_DIR/.ssh
