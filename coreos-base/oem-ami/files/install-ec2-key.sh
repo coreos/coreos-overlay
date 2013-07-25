f57053493652fd554e13eb065802ee341d2681d2
Alex Polvi <alex@polvi.net>
2013-07-25 09:40:58 -0700

    Merge branch 'master' of https://github.com/coreos/coreos-overlay into oem-ami
    Conflicts:
    	coreos-base/coreos/coreos-0.0.1-r196.ebuild
    	coreos-base/coreos/coreos-0.0.1-r199.ebuild
    	coreos-base/coreos/coreos-0.0.1-r200.ebuild
    	coreos-base/coreos/coreos-0.0.1.ebuild
    	coreos-base/oem-ami/files/install-ec2-key.sh

--- coreos-base/oem-ami/files/install-ec2-key.sh
+++ coreos-base/oem-ami/files/install-ec2-key.sh
@@ -8,8 +8,7 @@ if [ ! -d ${USER_DIR}/.ssh ] ; then
 	mkdir -p ${USER_DIR}/.ssh
 	chmod 700 ${USER_DIR}/.ssh
 fi
-# Fetch public key using HTTP
-curl -s --connect-timeout 3 http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key > /tmp/my-key
+curl -s http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key > /tmp/my-key
 if [ $? -eq 0 ] ; then
 	cat /tmp/my-key >> ${USER_DIR}/.ssh/authorized_keys
 	chmod 700 ${USER_DIR}/.ssh/authorized_keys
