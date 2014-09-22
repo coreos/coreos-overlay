coreos/coreos-overlay

commit 92dae274335eabf15705f7d6b945947250ef8a12

omaha_request: strip out channel switching logic
Under CoreOS update_engine is only ever aware of the group currently
listed in update.conf. There is no other API or other mechanism to
change the group. This means there is no distinction between
`current_channel` and `target_channel`. Additionally we do not support
downgrading so we have no use for "power washing" beforehand.

omaha_request_params.cc
@@ -31,178 +31,38 @@ const char* const OmahaRequestParams::kAppId(
     "{e96281a6-d1af-4bde-9a0a-97b76e56dc57}");
 const char* const OmahaRequestParams::kOsPlatform("CoreOS");
 const char* const OmahaRequestParams::kOsVersion("Chateau");
+const char* const OmahaRequestParams::kDefaultChannel("stable");
 const char* const kProductionOmahaUrl(
     "https://public.update.core-os.net/v1/update/");
 
-const char* const OmahaRequestParams::kUpdateChannelKey(
-    "GROUP");
-const char* const OmahaRequestParams::kIsPowerwashAllowedKey(
-    "CHROMEOS_IS_POWERWASH_ALLOWED");
-
-const char* kChannelsByStability[] = {
-    // This list has to be sorted from least stable to most stable channel.
-    "canary-channel",
-    "dev-channel",
-    "beta-channel",
-    "stable-channel",
-};
-
 bool OmahaRequestParams::Init(bool interactive) {
-  InitFromLsbValue();
-  bool stateful_override = !ShouldLockDown();
   os_platform_ = OmahaRequestParams::kOsPlatform;
   os_version_ = OmahaRequestParams::kOsVersion;
   oemid_ = GetOemValue("ID", "");
-  app_version_ =
-      GetLsbValue("COREOS_RELEASE_VERSION", "", NULL, stateful_override);
+  app_version_ = GetConfValue("COREOS_RELEASE_VERSION", "");
   os_sp_ = app_version_ + "_" + GetMachineType();
-  os_board_ = GetLsbValue("COREOS_RELEASE_BOARD",
-                          "",
-                          NULL,
-                          stateful_override);
-  app_id_ = GetLsbValue("COREOS_RELEASE_APPID",
-                        OmahaRequestParams::kAppId,
-                        NULL,
-                        stateful_override);
+  os_board_ = GetConfValue("COREOS_RELEASE_BOARD", "");
+  app_id_ = GetConfValue("COREOS_RELEASE_APPID", OmahaRequestParams::kAppId);
   app_lang_ = "en-US";
   bootid_ = utils::GetBootId();
   machineid_ = utils::GetMachineId();
-  update_url_ = GetLsbValue("SERVER", kProductionOmahaUrl, NULL,
-                            stateful_override);
+  update_url_ = GetConfValue("SERVER", kProductionOmahaUrl);
   interactive_ = interactive;
 
-  if (current_channel_ == target_channel_) {
-    // deltas are only okay if the /.nodelta file does not exist.  if we don't
-    // know (i.e. stat() returns some unexpected error), then err on the side of
-    // caution and say deltas are not okay.
-    struct stat stbuf;
-    delta_okay_ = (stat((root_ + "/.nodelta").c_str(), &stbuf) < 0) &&
-                  (errno == ENOENT);
+  app_channel_ = GetConfValue("GROUP", kDefaultChannel);
+  LOG(INFO) << "Current group set to " << app_channel_;
 
-  } else {
-    LOG(INFO) << "Disabling deltas as a channel change is pending";
-    // For now, disable delta updates if the current channel is different from
-    // the channel that we're sending to the update server because such updates
-    // are destined to fail -- the current rootfs hash will be different than
-    // the expected hash due to the different channel in /etc/lsb-release.
-    delta_okay_ = false;
-  }
+  // TODO: deltas can only be enabled if verity is active.
+  delta_okay_ = false;
 
   return true;
 }
 
-bool OmahaRequestParams::SetTargetChannel(const std::string& new_target_channel,
-                                          bool is_powerwash_allowed) {
-  LOG(INFO) << "SetTargetChannel called with " << new_target_channel
-            << ". Is Powerwash Allowed = "
-            << utils::ToString(is_powerwash_allowed);
-
-  // Ignore duplicate calls so we can make the method succeed and be
-  // idempotent so as not to surface unnecessary errors to the UI.
-  if (new_target_channel == target_channel_ &&
-      is_powerwash_allowed == is_powerwash_allowed_) {
-    if (new_target_channel == current_channel_) {
-      // Return true to make such calls no-op and idempotent.
-      LOG(INFO) << "SetTargetChannel: Already on " << current_channel_;
-      return true;
-    }
-
-    LOG(INFO) << "SetTargetChannel: Target channel has already been set";
-    return true;
-  }
-
-  // See if there's a channel change already in progress. If so, don't honor
-  // a new channel change until the existing request is fulfilled.
-  if (current_channel_ != target_channel_) {
-    // Avoid dealing with multiple pending channels as they cause a lot of
-    // edge cases that's not worth adding the complexity for.
-    LOG(ERROR) << "Cannot change to " << new_target_channel
-               << " now as we're currently in " << current_channel_
-               << " and the request to change to " << target_channel_
-               << " is pending";
-    return false;
-  }
-
-  TEST_AND_RETURN_FALSE(IsValidChannel(new_target_channel));
-  FilePath kFile(root_ + utils::kStatefulPartition + "/etc/coreos/update.conf");
-  string file_data;
-  map<string, string> data;
-  if (file_util::ReadFileToString(kFile, &file_data)) {
-    data = simple_key_value_store::ParseString(file_data);
-  }
-  data[kUpdateChannelKey] = new_target_channel;
-  data[kIsPowerwashAllowedKey] = is_powerwash_allowed ? "true" : "false";
-  file_data = simple_key_value_store::AssembleString(data);
-  TEST_AND_RETURN_FALSE(file_util::CreateDirectory(kFile.DirName()));
-  TEST_AND_RETURN_FALSE(
-      file_util::WriteFile(kFile, file_data.data(), file_data.size()) ==
-      static_cast<int>(file_data.size()));
-  target_channel_ = new_target_channel;
-  is_powerwash_allowed_ = is_powerwash_allowed;
-  return true;
-}
-
-void OmahaRequestParams::SetTargetChannelFromLsbValue() {
-  string target_channel_new_value = GetLsbValue(
-      kUpdateChannelKey,
-      "",
-      &chromeos_update_engine::OmahaRequestParams::IsValidChannel,
-      true);  // stateful_override
-
-  if (target_channel_ != target_channel_new_value) {
-    target_channel_ = target_channel_new_value;
-    LOG(INFO) << "Target Channel set to " << target_channel_
-              << " from LSB file";
-  }
-}
-
-void OmahaRequestParams::SetCurrentChannelFromLsbValue() {
-  string current_channel_new_value = GetLsbValue(
-      kUpdateChannelKey,
-      "",
-      NULL,  // No need to validate the read-only rootfs channel.
-      false);  // stateful_override is false so we get the current channel.
-
-  if (current_channel_ != current_channel_new_value) {
-    current_channel_ = current_channel_new_value;
-    LOG(INFO) << "Current Channel set to " << current_channel_
-              << " from LSB file in rootfs";
-  }
-}
-
-void OmahaRequestParams::SetIsPowerwashAllowedFromLsbValue() {
-  string is_powerwash_allowed_str = GetLsbValue(
-      kIsPowerwashAllowedKey,
-      "false",
-      NULL, // no need to validate
-      true); // always get it from stateful, as that's the only place it'll be
-  bool is_powerwash_allowed_new_value = (is_powerwash_allowed_str == "true");
-  if (is_powerwash_allowed_ != is_powerwash_allowed_new_value) {
-    is_powerwash_allowed_ = is_powerwash_allowed_new_value;
-    LOG(INFO) << "Powerwash Allowed set to "
-              << utils::ToString(is_powerwash_allowed_)
-              << " from LSB file in stateful";
-  }
-}
-
-void OmahaRequestParams::InitFromLsbValue() {
-  SetTargetChannelFromLsbValue();
-  SetCurrentChannelFromLsbValue();
-  SetIsPowerwashAllowedFromLsbValue();
-}
-
-string OmahaRequestParams::GetLsbValue(const string& key,
-                                       const string& default_value,
-                                       ValueValidator validator,
-                                       bool stateful_override) const {
-  vector<string> files;
-  files.push_back("/etc/coreos/update.conf");
-  files.push_back("/usr/share/coreos/update.conf");
-  files.push_back("/usr/share/coreos/release");
+string OmahaRequestParams::SearchConfValue(const vector<string>& files,
+                                           const string& key,
+                                           const string& default_value) const {
   for (vector<string>::const_iterator it = files.begin();
        it != files.end(); ++it) {
-    // TODO(adlr): make sure files checked are owned as root (and all their
-    // parents are recursively, too).
     string file_data;
     if (!utils::ReadFile(root_ + *it, &file_data))
       continue;
@@ -210,9 +70,6 @@ string OmahaRequestParams::GetLsbValue(const string& key,
     map<string, string> data = simple_key_value_store::ParseString(file_data);
     if (utils::MapContainsKey(data, key)) {
       const string& value = data[key];
-      if (validator && !CALL_MEMBER_FN(*this, validator)(value)) {
-        continue;
-      }
       return value;
     }
   }
@@ -220,20 +77,20 @@ string OmahaRequestParams::GetLsbValue(const string& key,
   return default_value;
 }
 
+string OmahaRequestParams::GetConfValue(const string& key,
+                                        const string& default_value) const {
+  vector<string> files;
+  files.push_back("/etc/coreos/update.conf");
+  files.push_back("/usr/share/coreos/update.conf");
+  files.push_back("/usr/share/coreos/release");
+  return SearchConfValue(files, key, default_value);
+}
+
 string OmahaRequestParams::GetOemValue(const string& key,
                                        const string& default_value) const {
-  string file_data;
-
-  if (!utils::ReadFile(root_ + "/etc/oem-release", &file_data))
-    return default_value;
-
-  map<string, string> data = simple_key_value_store::ParseString(file_data);
-  if (utils::MapContainsKey(data, key)) {
-    const string& value = data[key];
-    return value;
-  }
-
-  return default_value;
+  vector<string> files;
+  files.push_back("/etc/coreos/update.conf");
+  return SearchConfValue(files, key, default_value);
 }
 
 string OmahaRequestParams::GetMachineType() const {
@@ -244,40 +101,9 @@ string OmahaRequestParams::GetMachineType() const {
   return ret;
 }
 
-bool OmahaRequestParams::ShouldLockDown() const {
-  if (force_lock_down_) {
-    return forced_lock_down_;
-  }
-  return utils::IsOfficialBuild() && utils::IsNormalBootMode();
-}
-
-bool OmahaRequestParams::IsValidChannel(const std::string& channel) const {
-  return GetChannelIndex(channel) >= 0;
-}
-
 void OmahaRequestParams::set_root(const std::string& root) {
   root_ = root;
-  InitFromLsbValue();
-}
-
-void OmahaRequestParams::SetLockDown(bool lock) {
-  force_lock_down_ = true;
-  forced_lock_down_ = lock;
-}
-
-int OmahaRequestParams::GetChannelIndex(const std::string& channel) const {
-  for (size_t t = 0; t < arraysize(kChannelsByStability); ++t)
-    if (channel == kChannelsByStability[t])
-      return t;
-
-  return -1;
-}
-
-bool OmahaRequestParams::to_more_stable_channel() const {
-  int current_channel_index = GetChannelIndex(current_channel_);
-  int target_channel_index = GetChannelIndex(target_channel_);
-
-  return target_channel_index > current_channel_index;
+  Init(false);
 }
 
 }  // namespace chromeos_update_engine

install_plan.cc
@@ -26,15 +26,13 @@ InstallPlan::InstallPlan(bool is_resume,
       kernel_install_path(kernel_install_path),
       kernel_size(0),
       rootfs_size(0),
-      hash_checks_mandatory(false),
-      powerwash_required(false) {}
+      hash_checks_mandatory(false) {}
 
 InstallPlan::InstallPlan() : is_resume(false),
                              payload_size(0),
                              kernel_size(0),
                              rootfs_size(0),
-                             hash_checks_mandatory(false),
-                             powerwash_required(false) {}
+                             hash_checks_mandatory(false) {}
 
 
 bool InstallPlan::operator==(const InstallPlan& that) const {
@@ -59,9 +57,7 @@ void InstallPlan::Dump() const {
             << ", install_path: " << install_path
             << ", kernel_install_path: " << kernel_install_path
             << ", hash_checks_mandatory: " << utils::ToString(
-                hash_checks_mandatory)
-            << ", powerwash_required: " << utils::ToString(
-                powerwash_required);
+                hash_checks_mandatory);
 }
 
 }  // namespace chromeos_update_engine

install_plan.h
@@ -58,10 +58,6 @@ struct InstallPlan {
   // True if payload hash checks are mandatory based on the system state and
   // the Omaha response.
   bool hash_checks_mandatory;
-
-  // True if Powerwash is required on reboot after applying the payload.
-  // False otherwise.
-  bool powerwash_required;
 };
 
 }  // namespace chromeos_update_engine

omaha_request_action.cc
@@ -155,37 +155,13 @@ string GetAppXml(const OmahaEvent* event,
                  SystemState* system_state) {
   string app_body = GetAppBody(event, params, ping_only, ping_active_days,
                                ping_roll_call_days, system_state->prefs());
-  string app_versions;
-
-  // If we are upgrading to a more stable channel and we are allowed to do
-  // powerwash, then pass 0.0.0.0 as the version. This is needed to get the
-  // highest-versioned payload on the destination channel.
-  if (params.to_more_stable_channel() && params.is_powerwash_allowed()) {
-    LOG(INFO) << "Passing OS version as 0.0.0.0 as we are set to powerwash "
-              << "on downgrading to the version in the more stable channel";
-    app_versions = "version=\"0.0.0.0\" from_version=\"" +
-      XmlEncode(params.app_version()) + "\" ";
-  } else {
-    app_versions = "version=\"" + XmlEncode(params.app_version()) + "\" ";
-  }
-
-  string app_channels = "";
-  if (params.target_channel().length() > 0) {
-     app_channels += "track=\"" + XmlEncode(params.target_channel()) + "\" ";
-  } else {
-     app_channels += "track=\"" + XmlEncode(params.current_channel()) + "\" ";
-  }
-
-  if (params.current_channel() != params.target_channel())
-     app_channels +=
-       "from_track=\"" + XmlEncode(params.current_channel()) + "\" ";
 
   string delta_okay_str = params.delta_okay() ? "true" : "false";
 
   string app_xml =
       "    <app appid=\"" + XmlEncode(params.app_id()) + "\" " +
-                app_versions +
-                app_channels +
+                "version=\"" + XmlEncode(params.app_version()) + "\" " +
+                "track=\"" + XmlEncode(params.app_channel()) + "\" " +
                 "bootid=\"" + XmlEncode(params.bootid()) + "\" " +
                 "oem=\"" + XmlEncode(params.oemid()) + "\" " +
                 "machineid=\"" + XmlEncode(params.machineid()) + "\" " +

omaha_request_params.h
@@ -6,6 +6,7 @@
 #define CHROMEOS_PLATFORM_UPDATE_ENGINE_OMAHA_REQUEST_PARAMS_H__
 
 #include <string>
+#include <vector>
 
 #include <base/basictypes.h>
 #include <base/time.h>
@@ -34,18 +35,14 @@ class OmahaRequestParams {
         os_platform_(kOsPlatform),
         os_version_(kOsVersion),
         app_id_(kAppId),
+	app_channel_(kDefaultChannel),
         delta_okay_(true),
         interactive_(false),
         update_disabled_(false),
         wall_clock_based_wait_enabled_(false),
         update_check_count_wait_enabled_(false),
         min_update_checks_needed_(kDefaultMinUpdateChecks),
-        max_update_checks_allowed_(kDefaultMaxUpdateChecks),
-        is_powerwash_allowed_(false),
-        force_lock_down_(false),
-        forced_lock_down_(false) {
-    InitFromLsbValue();
-  }
+        max_update_checks_allowed_(kDefaultMaxUpdateChecks) {}
 
   OmahaRequestParams(SystemState* system_state,
                      const std::string& in_os_platform,
@@ -55,7 +52,7 @@ class OmahaRequestParams {
                      const std::string& in_app_id,
                      const std::string& in_app_version,
                      const std::string& in_app_lang,
-                     const std::string& in_target_channel,
+                     const std::string& in_app_channel,
                      const std::string& in_hwid,
                      const std::string& in_bootid,
                      bool in_delta_okay,
@@ -71,8 +68,7 @@ class OmahaRequestParams {
         app_id_(in_app_id),
         app_version_(in_app_version),
         app_lang_(in_app_lang),
-        current_channel_(in_target_channel),
-        target_channel_(in_target_channel),
+        app_channel_(in_app_channel),
         hwid_(in_hwid),
         bootid_(in_bootid),
         delta_okay_(in_delta_okay),
@@ -83,10 +79,7 @@ class OmahaRequestParams {
         wall_clock_based_wait_enabled_(false),
         update_check_count_wait_enabled_(false),
         min_update_checks_needed_(kDefaultMinUpdateChecks),
-        max_update_checks_allowed_(kDefaultMaxUpdateChecks),
-        is_powerwash_allowed_(false),
-        force_lock_down_(false),
-        forced_lock_down_(false) {}
+        max_update_checks_allowed_(kDefaultMaxUpdateChecks) {}
 
   // Setters and getters for the various properties.
   inline std::string os_platform() const { return os_platform_; }
@@ -104,9 +97,7 @@ class OmahaRequestParams {
     app_version_ = version;
   }
   inline std::string app_version() const { return app_version_; }
-
-  inline std::string current_channel() const { return current_channel_; }
-  inline std::string target_channel() const { return target_channel_; }
+  inline std::string app_channel() const { return app_channel_; }
 
   // Can client accept a delta ?
   inline void set_delta_okay(bool ok) { delta_okay_ = ok; }
@@ -165,17 +156,12 @@ class OmahaRequestParams {
     return max_update_checks_allowed_;
   }
 
-  // True if we're trying to update to a more stable channel.
-  // i.e. index(target_channel) > index(current_channel).
-  bool to_more_stable_channel() const;
-
   // Suggested defaults
   static const char* const kAppId;
   static const char* const kOsPlatform;
   static const char* const kOsVersion;
   static const char* const kUpdateUrl;
-  static const char* const kUpdateChannelKey;
-  static const char* const kIsPowerwashAllowedKey;
+  static const char* const kDefaultChannel;
   static const int64 kDefaultMinUpdateChecks = 0;
   static const int64 kDefaultMaxUpdateChecks = 8;
 
@@ -183,69 +169,23 @@ class OmahaRequestParams {
   // Returns true on success, false otherwise.
   bool Init(bool interactive);
 
-  // Permanently changes the release channel to |channel|. Performs a
-  // powerwash, if required and allowed.
-  // Returns true on success, false otherwise. Note: This call will fail if
-  // there's a channel change pending already. This is to serialize all the
-  // channel changes done by the user in order to avoid having to solve
-  // numerous edge cases around ensuring the powerwash happens as intended in
-  // all such cases.
-  bool SetTargetChannel(const std::string& channel, bool is_powerwash_allowed);
-
-  bool is_powerwash_allowed() const { return is_powerwash_allowed_; }
-
   // For unit-tests.
   void set_root(const std::string& root);
 
-  // Enforce security mode for testing purposes.
-  void SetLockDown(bool lock);
-
  private:
-  FRIEND_TEST(OmahaRequestParamsTest, IsValidChannelTest);
-  FRIEND_TEST(OmahaRequestParamsTest, ShouldLockDownTest);
-  FRIEND_TEST(OmahaRequestParamsTest, ChannelIndexTest);
-  FRIEND_TEST(OmahaRequestParamsTest, LsbPreserveTest);
-
-  // Use a validator that is a non-static member of this class so that its
-  // inputs can be mocked in unit tests (e.g., build type for IsValidChannel).
-  typedef bool(OmahaRequestParams::*ValueValidator)(
-      const std::string&) const;
-
-  // Returns true if parameter values should be locked down for security
-  // reasons. If this is an official build running in normal boot mode, all
-  // values except the release channel are parsed only from the read-only rootfs
-  // partition and the channel values are restricted to a pre-approved set.
-  bool ShouldLockDown() const;
-
-  // Returns true if |channel| is a valid channel, false otherwise. This method
-  // restricts the channel value only if the image is official (see
-  // IsOfficialBuild).
-  bool IsValidChannel(const std::string& channel) const;
-
-  // Returns the index of the given channel.
-  int GetChannelIndex(const std::string& channel) const;
-
-  // These are individual helper methods to initialize the said properties from
-  // the LSB value.
-  void SetTargetChannelFromLsbValue();
-  void SetCurrentChannelFromLsbValue();
-  void SetIsPowerwashAllowedFromLsbValue();
-
-  // Initializes the required properties from the LSB value.
-  void InitFromLsbValue();
-
-  // Fetches the value for a given key from
-  // /media/state/etc/lsb-release if possible and |stateful_override|
-  // is true. Failing that, it looks for the key in /etc/lsb-release. If
-  // |validator| is non-NULL, uses it to validate and ignore invalid valies.
-  std::string GetLsbValue(const std::string& key,
-                          const std::string& default_value,
-                          ValueValidator validator,
-                          bool stateful_override) const;
+  // Fetches the value for a given key from update.conf files.
+  std::string GetConfValue(const std::string& key,
+                           const std::string& default_value) const;
 
   // Fetches the value for a given key from /etc/oem-release.
   std::string GetOemValue(const std::string& key,
                           const std::string& default_value) const;
+
+  // Common implementation of GetConfValue and GetOemValue.
+  std::string SearchConfValue(
+      const std::vector<std::string>& files,
+      const std::string& key,
+      const std::string& default_value) const;
 
   // Gets the machine type (e.g. "i686").
   std::string GetMachineType() const;
@@ -259,15 +199,10 @@ class OmahaRequestParams {
   std::string os_sp_;
   std::string os_board_;
 
-  // The app_id identifies CoreOS to the update service.
-  std::string app_id_;
+  std::string app_id_;	// The app_id identifies CoreOS to the update service.
   std::string app_version_;
   std::string app_lang_;
-
-  // Current channel and target channel. Usually there are same, except when
-  // there's a pending channel change.
-  std::string current_channel_;
-  std::string target_channel_;
+  std::string app_channel_; // Current update group, aka channel, aka track.
   std::string hwid_;  // Hardware Qualification ID of the client
   std::string bootid_;  // Kernel generated guid that identifies this boot
   std::string machineid_; // Unique machine ID that is set during installation 
@@ -299,15 +234,8 @@ class OmahaRequestParams {
   int64 min_update_checks_needed_;
   int64 max_update_checks_allowed_;
 
-  // True if we are allowed to do powerwash, if required, on a channel change.
-  bool is_powerwash_allowed_;
-
   // When reading files, prepend root_ to the paths. Useful for testing.
   std::string root_;
-
-  // Force security lock down for testing purposes.
-  bool force_lock_down_;
-  bool forced_lock_down_;
 
   // TODO(jaysri): Uncomment this after fixing unit tests, as part of
   // chromium-os:39752

omaha_request_params_unittest.cc
@@ -35,7 +35,6 @@ class OmahaRequestParamsTest : public ::testing::Test {
     OmahaRequestParams new_params(&mock_system_state);
     params_ = new_params;
     params_.set_root(string("./") + kTestDir);
-    params_.SetLockDown(false);
   }
 
   virtual void TearDown() {
@@ -85,8 +84,8 @@ TEST_F(OmahaRequestParamsTest, SimpleTest) {
   EXPECT_EQ("0.2.2.3", out.app_version());
   EXPECT_EQ("en-US", out.app_lang());
   EXPECT_EQ("", out.hwid());
-  EXPECT_TRUE(out.delta_okay());
-  EXPECT_EQ("dev-channel", out.target_channel());
+  EXPECT_FALSE(out.delta_okay());
+  EXPECT_EQ("dev-channel", out.app_channel());
   EXPECT_EQ("http://www.google.com", out.update_url());
 }
 
@@ -106,8 +105,8 @@ TEST_F(OmahaRequestParamsTest, AppIDTest) {
   EXPECT_EQ("0.2.2.3", out.app_version());
   EXPECT_EQ("en-US", out.app_lang());
   EXPECT_EQ("", out.hwid());
-  EXPECT_TRUE(out.delta_okay());
-  EXPECT_EQ("dev-channel", out.target_channel());
+  EXPECT_FALSE(out.delta_okay());
+  EXPECT_EQ("dev-channel", out.app_channel());
   EXPECT_EQ("http://www.google.com", out.update_url());
 }
 
@@ -124,7 +123,7 @@ TEST_F(OmahaRequestParamsTest, MissingChannelTest) {
   EXPECT_EQ("{e96281a6-d1af-4bde-9a0a-97b76e56dc57}", out.app_id());
   EXPECT_EQ("0.2.2.3", out.app_version());
   EXPECT_EQ("en-US", out.app_lang());
-  EXPECT_EQ("", out.target_channel());
+  EXPECT_EQ("stable", out.app_channel());
 }
 
 TEST_F(OmahaRequestParamsTest, ConfusingReleaseTest) {
@@ -140,7 +139,7 @@ TEST_F(OmahaRequestParamsTest, ConfusingReleaseTest) {
   EXPECT_EQ("{e96281a6-d1af-4bde-9a0a-97b76e56dc57}", out.app_id());
   EXPECT_EQ("0.2.2.3", out.app_version());
   EXPECT_EQ("en-US", out.app_lang());
-  EXPECT_EQ("", out.target_channel());
+  EXPECT_EQ("stable", out.app_channel());
 }
 
 TEST_F(OmahaRequestParamsTest, MissingVersionTest) {
@@ -155,8 +154,8 @@ TEST_F(OmahaRequestParamsTest, MissingVersionTest) {
   EXPECT_EQ("{e96281a6-d1af-4bde-9a0a-97b76e56dc57}", out.app_id());
   EXPECT_EQ("", out.app_version());
   EXPECT_EQ("en-US", out.app_lang());
-  EXPECT_TRUE(out.delta_okay());
-  EXPECT_EQ("dev-channel", out.target_channel());
+  EXPECT_FALSE(out.delta_okay());
+  EXPECT_EQ("dev-channel", out.app_channel());
 }
 
 TEST_F(OmahaRequestParamsTest, MissingURLTest) {
@@ -172,57 +171,9 @@ TEST_F(OmahaRequestParamsTest, MissingURLTest) {
   EXPECT_EQ("{e96281a6-d1af-4bde-9a0a-97b76e56dc57}", out.app_id());
   EXPECT_EQ("0.2.2.3", out.app_version());
   EXPECT_EQ("en-US", out.app_lang());
-  EXPECT_TRUE(out.delta_okay());
-  EXPECT_EQ("dev-channel", out.target_channel());
-  EXPECT_EQ(kProductionOmahaUrl, out.update_url());
-}
-
-TEST_F(OmahaRequestParamsTest, NoDeltasTest) {
-  ASSERT_TRUE(WriteFileString(
-      kTestDir + "/usr/share/coreos/release",
-      "COREOS_RELEASE_FOO=COREOS_RELEASE_VERSION=1.2.3.4\n"
-      "COREOS_RELEASE_VERSION=0.2.2.3\n"
-      "COREOS_RELEASE_TRXCK=dev-channel"));
-  ASSERT_TRUE(WriteFileString(kTestDir + "/.nodelta", ""));
-  MockSystemState mock_system_state;
-  OmahaRequestParams out(&mock_system_state);
-  EXPECT_TRUE(DoTest(&out));
   EXPECT_FALSE(out.delta_okay());
-}
-
-TEST_F(OmahaRequestParamsTest, SetTargetChannelInvalidTest) {
-  ASSERT_TRUE(WriteFileString(
-      kTestDir + "/usr/share/coreos/release",
-      "COREOS_RELEASE_FOO=bar\n"
-      "COREOS_RELEASE_VERSION=0.2.2.3\n"
-      "GROUP=dev-channel\n"
-      "SERVER=http://www.google.com"));
-  {
-    MockSystemState mock_system_state;
-    OmahaRequestParams params(&mock_system_state);
-    params.set_root(string("./") + kTestDir);
-    params.SetLockDown(true);
-    EXPECT_TRUE(params.Init(false));
-    params.SetTargetChannel("dogfood-channel", true);
-    EXPECT_FALSE(params.is_powerwash_allowed());
-  }
-  MockSystemState mock_system_state;
-  OmahaRequestParams out(&mock_system_state);
-  EXPECT_TRUE(DoTest(&out));
-  EXPECT_EQ("dev-channel", out.target_channel());
-  EXPECT_FALSE(out.is_powerwash_allowed());
-}
-
-TEST_F(OmahaRequestParamsTest, IsValidChannelTest) {
-  params_.SetLockDown(false);
-  EXPECT_TRUE(params_.IsValidChannel("canary-channel"));
-  EXPECT_TRUE(params_.IsValidChannel("stable-channel"));
-  EXPECT_TRUE(params_.IsValidChannel("beta-channel"));
-  EXPECT_TRUE(params_.IsValidChannel("dev-channel"));
-  EXPECT_FALSE(params_.IsValidChannel("testimage-channel"));
-  EXPECT_FALSE(params_.IsValidChannel("dogfood-channel"));
-  EXPECT_FALSE(params_.IsValidChannel("some-channel"));
-  EXPECT_FALSE(params_.IsValidChannel(""));
+  EXPECT_EQ("dev-channel", out.app_channel());
+  EXPECT_EQ(kProductionOmahaUrl, out.update_url());
 }
 
 TEST_F(OmahaRequestParamsTest, ValidChannelTest) {
@@ -232,7 +183,6 @@ TEST_F(OmahaRequestParamsTest, ValidChannelTest) {
       "COREOS_RELEASE_VERSION=0.2.2.3\n"
       "GROUP=dev-channel\n"
       "SERVER=http://www.google.com"));
-  params_.SetLockDown(true);
   MockSystemState mock_system_state;
   OmahaRequestParams out(&mock_system_state);
   EXPECT_TRUE(DoTest(&out));
@@ -241,29 +191,9 @@ TEST_F(OmahaRequestParamsTest, ValidChannelTest) {
   EXPECT_EQ("0.2.2.3", out.app_version());
   EXPECT_EQ("en-US", out.app_lang());
   EXPECT_EQ("", out.hwid());
-  EXPECT_TRUE(out.delta_okay());
-  EXPECT_EQ("dev-channel", out.target_channel());
+  EXPECT_FALSE(out.delta_okay());
+  EXPECT_EQ("dev-channel", out.app_channel());
   EXPECT_EQ("http://www.google.com", out.update_url());
-}
-
-TEST_F(OmahaRequestParamsTest, ChannelIndexTest) {
-  int canary = params_.GetChannelIndex("canary-channel");
-  int dev = params_.GetChannelIndex("dev-channel");
-  int beta = params_.GetChannelIndex("beta-channel");
-  int stable = params_.GetChannelIndex("stable-channel");
-  EXPECT_LE(canary, dev);
-  EXPECT_LE(dev, beta);
-  EXPECT_LE(beta, stable);
-
-  // testimage-channel or other names are not recognized, so index will be -1.
-  int testimage = params_.GetChannelIndex("testimage-channel");
-  int bogus = params_.GetChannelIndex("bogus-channel");
-  EXPECT_EQ(-1, testimage);
-  EXPECT_EQ(-1, bogus);
-}
-
-TEST_F(OmahaRequestParamsTest, ShouldLockDownTest) {
-  EXPECT_FALSE(params_.ShouldLockDown());
 }
 
 }  // namespace chromeos_update_engine

commit 6587fbb89675af9083bd33a331582898eb7ec9fc

omaha_request_params: fix reading OEM attributes
This was broken in commit 92dae274 during some refactoring.

omaha_request_params.cc
@@ -89,7 +89,7 @@ string OmahaRequestParams::GetConfValue(const string& key,
 string OmahaRequestParams::GetOemValue(const string& key,
                                        const string& default_value) const {
   vector<string> files;
-  files.push_back("/etc/coreos/update.conf");
+  files.push_back("/etc/oem-release");
   return SearchConfValue(files, key, default_value);
 }
 