coreos/coreos-overlay

commit 3f0280600acc37f2df0842845361fb2d00028b8e

coreos-firmware: update to new snapshot, cleanup version detection
Now that the linux-info eclass works we can clean out some of the
version detection goo in coreos-firmware. Update snapshot for 3.18

sys-kernel/coreos-firmware/Manifest
@@ -1,3 +1,4 @@
 DIST linux-firmware-20130728.tar.xz 19243912 SHA256 dff71cb96eb286e4cacc0d0fb0337481c285f091b23372a77c147e7125f46a9a SHA512 c86e003177988e58b6eae632d1e7f8022c5e2c848e83b925c1852dc87bce3eb9b94bccf8d33c67f73dc355cf4b02c231dadc53300b6a26ee0e8ca75878059609 WHIRLPOOL a8de4a07d1a48ba2b45d1a9143a015ead11d85498836c3c946fa0386099e66d0f71b05e70a6f230832d19401884f21a4b818b75a873e8b9caf5a19eec5067389
 DIST linux-firmware-20131230.tar.xz 21214036 SHA256 7189fd4cf954086a349286ca79d9275f6a2d79a7c0fd5c7cf6d41d25a863cc77 SHA512 4fe92db99cfd5bc0806ef5863fa6709501dc2c761b9af92689ff15fa8f9a97ca6086bbb3a79fa97e6bde15f8f67c0ae06eb38323fe5182f5406cf9601c4e6b57 WHIRLPOOL aaac2efb4a205bba80c3bad9c6b7eb38ec4fcde5b0233952eb1f58b6cc6afcf5059fc3738ea8a6f9e02de937bcaaf866ddf8021614ccc2cb067b665466ce2ea5
 DIST linux-firmware-20140603.tar.xz 23299316 SHA256 f45cb7aa37fd52f65289389ee5b112235e8044bd6f0926f070dfee8c8118b749 SHA512 93add92b2b25fd5001e84e66b4c3e28be4ba99827d45758bc8d251ee359d09d88ae8057978a800b26d32e7711bab14a19191a53fc48faf47b76e172b8d07f464 WHIRLPOOL 6cf7a6d845f545ec176994788ee0bc25b884f33a5d24f599757ce5e6e2cfeae47c796ef968c44f5c6de74c3fa44614b9c7056453923f31275f7b877865bf76e8
+DIST linux-firmware-20141009.tar.xz 24288396 SHA256 b3423bc48bdb46d89707096d9bed3eb0e59ec9172b52ad61d49d996f49f09f61 SHA512 d599abc518e95834118311428fd5cb3e5de47a25a264397758f8456bb3e1e1ed751d0fbfccc5196bd153c1fa4c0e477a7e2e6706f44ef1ba83bcab94b34d5ca3 WHIRLPOOL fd9f13f951c17fcff4db2981f0a71e91dde8b14639746d92ab51a16720758e1f3044a6c718580b5c662cb5a6e571e23739bceb7a8ea9ffe54d2492216efa4320

sys-kernel/coreos-firmware/coreos-firmware-20141009.ebuild
@@ -1,0 +1,1 @@
+coreos-firmware-99999999.ebuild

sys-kernel/coreos-firmware/coreos-firmware-99999999.ebuild
@@ -4,6 +4,11 @@
 
 EAPI=5
 
+# Tell linux-info where to find the kernel source/build
+KERNEL_DIR="${SYSROOT}/usr/src/linux"
+KBUILD_OUTPUT="${SYSROOT}/var/cache/portage/sys-kernel/coreos-kernel"
+inherit linux-info
+
 if [[ ${PV} == 99999999* ]]; then
 	inherit git-2
 	SRC_URI=""
@@ -21,10 +26,9 @@ LICENSE="GPL-1 GPL-2 GPL-3 BSD freedist"
 SLOT="0"
 IUSE=""
 
-DEPEND="sys-kernel/coreos-kernel:="
+DEPEND="sys-kernel/coreos-sources
+	>=sys-kernel/coreos-kernel-3.17.6-r1:="
 RDEPEND="${DEPEND}
-	!=sys-kernel/coreos-kernel-3.12.6
-	!<=sys-kernel/coreos-kernel-3.11.7-r5
 	!sys-kernel/linux-firmware
 	!sys-firmware/alsa-firmware[alsa_cards_ca0132]
 	!sys-firmware/alsa-firmware[alsa_cards_korg1212]
@@ -76,22 +80,13 @@ src_unpack() {
 }
 
 src_prepare() {
-	# FIXME(marineam): The linux-info eclass would normally be able to
-	# provide version info but since we don't install kernel source code the
-	# way it expects it doesn't work correctly. Will fix this eventually...
-	local kernel_pkg=$(best_version sys-kernel/coreos-kernel)
-	local kernel_ver="${kernel_pkg#sys-kernel/coreos-kernel-}"
-	kernel_ver="${kernel_ver%_*}"
-	kernel_ver="${kernel_ver%-*}"
-	local kernel_mods="${ROOT}/lib/modules/${kernel_ver}"
-	# the actually version may have a + or other extraversion suffix
-	local kernel_mods=$(ls -d1 "${kernel_mods}"* | tail -n1)
+	local kernel_mods="${ROOT}/lib/modules/${KV_FULL}"
 
 	# If any firmware is missing warn but don't raise a fuss. Missing
 	# files either means linux-firmware probably out-of-date but since
 	# this is new and hacky I'm not going to worry too much just yet.
 
-	einfo "Scanning for files required by ${kernel_pkg}"
+	einfo "Scanning for files required by ${KV_FULL}"
 	echo -n > "${T}/firmware-scan"
 	local kofile fwfile
 	for kofile in $(find "${kernel_mods}" -name '*.ko'); do

commit ae6051f109c6a5aff70142a7e211b1622dd7aff9

coreos-firmware: remove coreos-sources from runtime dependencies
Fixes commit 3f028060 which mistakenly added coreos-sources as both
build and run-time dependencies. This was missed initially because there
are exclude rules that remove /usr/src from production images but it
does needlessly slow down the build and pulls in extraneous kernel build
dependencies like perl.

Prune old releases, no need for them.

sys-kernel/coreos-firmware/Manifest
@@ -1,4 +1,1 @@
-DIST linux-firmware-20130728.tar.xz 19243912 SHA256 dff71cb96eb286e4cacc0d0fb0337481c285f091b23372a77c147e7125f46a9a SHA512 c86e003177988e58b6eae632d1e7f8022c5e2c848e83b925c1852dc87bce3eb9b94bccf8d33c67f73dc355cf4b02c231dadc53300b6a26ee0e8ca75878059609 WHIRLPOOL a8de4a07d1a48ba2b45d1a9143a015ead11d85498836c3c946fa0386099e66d0f71b05e70a6f230832d19401884f21a4b818b75a873e8b9caf5a19eec5067389
-DIST linux-firmware-20131230.tar.xz 21214036 SHA256 7189fd4cf954086a349286ca79d9275f6a2d79a7c0fd5c7cf6d41d25a863cc77 SHA512 4fe92db99cfd5bc0806ef5863fa6709501dc2c761b9af92689ff15fa8f9a97ca6086bbb3a79fa97e6bde15f8f67c0ae06eb38323fe5182f5406cf9601c4e6b57 WHIRLPOOL aaac2efb4a205bba80c3bad9c6b7eb38ec4fcde5b0233952eb1f58b6cc6afcf5059fc3738ea8a6f9e02de937bcaaf866ddf8021614ccc2cb067b665466ce2ea5
-DIST linux-firmware-20140603.tar.xz 23299316 SHA256 f45cb7aa37fd52f65289389ee5b112235e8044bd6f0926f070dfee8c8118b749 SHA512 93add92b2b25fd5001e84e66b4c3e28be4ba99827d45758bc8d251ee359d09d88ae8057978a800b26d32e7711bab14a19191a53fc48faf47b76e172b8d07f464 WHIRLPOOL 6cf7a6d845f545ec176994788ee0bc25b884f33a5d24f599757ce5e6e2cfeae47c796ef968c44f5c6de74c3fa44614b9c7056453923f31275f7b877865bf76e8
 DIST linux-firmware-20141009.tar.xz 24288396 SHA256 b3423bc48bdb46d89707096d9bed3eb0e59ec9172b52ad61d49d996f49f09f61 SHA512 d599abc518e95834118311428fd5cb3e5de47a25a264397758f8456bb3e1e1ed751d0fbfccc5196bd153c1fa4c0e477a7e2e6706f44ef1ba83bcab94b34d5ca3 WHIRLPOOL fd9f13f951c17fcff4db2981f0a71e91dde8b14639746d92ab51a16720758e1f3044a6c718580b5c662cb5a6e571e23739bceb7a8ea9ffe54d2492216efa4320

sys-kernel/coreos-firmware/coreos-firmware-20131230.ebuild
@@ -1,1 +1,0 @@
-coreos-firmware-99999999.ebuild

sys-kernel/coreos-firmware/coreos-firmware-20140603.ebuild
@@ -1,1 +1,0 @@
-coreos-firmware-99999999.ebuild

sys-kernel/coreos-firmware/coreos-firmware-20141009.ebuild
@@ -1,1 +1,0 @@
-coreos-firmware-99999999.ebuild

sys-kernel/coreos-firmware/coreos-firmware-99999999.ebuild
@@ -26,9 +26,10 @@ LICENSE="GPL-1 GPL-2 GPL-3 BSD freedist"
 SLOT="0"
 IUSE=""
 
-DEPEND="sys-kernel/coreos-sources
-	>=sys-kernel/coreos-kernel-3.17.6-r1:="
-RDEPEND="${DEPEND}
+CDEPEND=">=sys-kernel/coreos-kernel-3.17.6-r1:="
+DEPEND="${CDEPEND}
+	sys-kernel/coreos-sources"
+RDEPEND="${CDEPEND}
 	!sys-kernel/linux-firmware
 	!sys-firmware/alsa-firmware[alsa_cards_ca0132]
 	!sys-firmware/alsa-firmware[alsa_cards_korg1212]