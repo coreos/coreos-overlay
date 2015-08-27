0d6f2f1a6b239fdbc4bbc6f92e1639ae7f28181e
David Laube <dlaube@packethost.net>
2015-08-27 09:09:05 -0700

    Abstracting interface types for Packet hardware

--- coreos-base/oem-packet/files/reset-interfaces.sh
+++ coreos-base/oem-packet/files/reset-interfaces.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-
 logger "Hard reset of all network interfaces due to upstream systemd-networkd issue: https://github.com/coreos/bugs/issues/36 ."
-ip link set enp1s0f0 down
-ip link set enp1s0f1 down
+for iface in `ls -1 /sys/class/net | egrep -v "(^lo$|^bond)"`; do
+	ip link set $iface down
+done
 ip link del bond0
 systemctl restart systemd-networkd
