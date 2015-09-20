ef94c834bfde26769473a69ac062b4825afdffc8
Michael Marineau <michael.marineau@coreos.com>
2015-09-20 16:54:38 -0700

    scoped_temp_dir: fix missing include, add to build

--- src/files/scoped_temp_dir.h
+++ src/files/scoped_temp_dir.h
@@ -15,6 +15,8 @@
 // (CreateUniqueTempDir, CreateUniqueTempDirUnderPath, and Set) must have
 // intervening calls to Delete or Take, or the calls will fail.
 
+#include "base/basictypes.h"
+
 #include "files/file_path.h"
 
 namespace files {
