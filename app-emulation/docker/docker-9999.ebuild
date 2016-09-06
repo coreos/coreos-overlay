coreos/coreos-overlay

commit b9a6d189d075840c9b20933306b7098ddc9da50a

app-emulation/docker: backport stats api fix

app-emulation/docker/docker-9999.ebuild
@@ -12,7 +12,7 @@ if [[ ${PV} == *9999 ]]; then
 	DOCKER_GITCOMMIT="unknown"
 	KEYWORDS="~amd64 ~arm64"
 else
-	CROS_WORKON_COMMIT="4a6e2b1e56ecd816ca066f71e7632f8a0171cada" # coreos-1.11.2
+	CROS_WORKON_COMMIT="eff1e6980f318e3745bbdc066f8728446694b4be" # coreos-1.11.2
 	DOCKER_GITCOMMIT="${CROS_WORKON_COMMIT:0:7}"
 	KEYWORDS="amd64 arm64"
 fi

commit 62bc959bb9a3324bad493aa0daa5cd18f56d425e

app-emulation/docker: use correct git revision
Broken in b9a6d189. Not sure where that revision came from.

app-emulation/docker/docker-9999.ebuild
@@ -12,7 +12,7 @@ if [[ ${PV} == *9999 ]]; then
 	DOCKER_GITCOMMIT="unknown"
 	KEYWORDS="~amd64 ~arm64"
 else
-	CROS_WORKON_COMMIT="eff1e6980f318e3745bbdc066f8728446694b4be" # coreos-1.11.2
+	CROS_WORKON_COMMIT="82a3ad741782a008a14118eb5be3ac5be33c603c" # coreos-1.11.2
 	DOCKER_GITCOMMIT="${CROS_WORKON_COMMIT:0:7}"
 	KEYWORDS="amd64 arm64"
 fi