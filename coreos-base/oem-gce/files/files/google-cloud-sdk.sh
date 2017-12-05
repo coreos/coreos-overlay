5e92f94f69b1acb3db3da9723a77f1565e6c387c
Benjamin Gilbert <benjamin.gilbert@coreos.com>
2017-12-04 17:36:03 -0800

    coreos-base/oem-gce: drop extra quotes from shell aliases
    They aren't necessary here and result in quoting that doesn't do
    what it appears to do.

--- coreos-base/oem-gce/files/files/google-cloud-sdk.sh
+++ coreos-base/oem-gce/files/files/google-cloud-sdk.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-alias gcloud="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -ti --rm --net="host" -v $HOME/.config:/root/.config -v /var/run/docker.sock:/var/run/docker.sock google/cloud-sdk gcloud"
-alias gsutil="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -ti --rm --net="host" -v $HOME/.config:/root/.config google/cloud-sdk gsutil"
+alias gcloud="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -ti --rm --net=host -v $HOME/.config:/root/.config -v /var/run/docker.sock:/var/run/docker.sock google/cloud-sdk gcloud"
+alias gsutil="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -ti --rm --net=host -v $HOME/.config:/root/.config google/cloud-sdk gsutil"
